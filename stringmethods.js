12e3ea8e3523d0f756821f711bcf654dce817c23
sibu02 <sibaprasad.tofan@gmail.com>
2024-06-29 18:03:13 +0530

    modified the file

--- stringmethods.js
+++ stringmethods.js
@@ -3,25 +3,26 @@
 
 // str.toUpperCase()  "abc"-->"ABC"
 
-// let str="hii this is tofan";
-// let str2=str.toUpperCase();
-// console.log(str2);
+let str="hii this is tofan";
+let str2=str.toUpperCase();
+console.log(str2);
 
 // str.toLocaleLowerCase()  "ABC"-->"abc"
 
-// let str3="Hii This IS TOFAN";
-// let str4=str3.toLowerCase();
-// console.log(str4);
+let str3="Hii This IS TOFAN";
+let str4=str3.toLowerCase();
+console.log(str4);
 
 /// str.trim()  "   This is tofan   "
-// let str5="   This is tofan   ";
-// console.log(str5.trim());
+
+let str5="   This is tofan   ";
+console.log(str5.trim());
 
 
 /// str.slice(start,end?) return part of string. The end part is optional. 
 
-// let str6="tofan";
-// console.log(str6.slice(1,4));
+let str6="tofan";
+console.log(str6.slice(1,4));
 
 /// str1.concat(str2) to jaion two string.
 
