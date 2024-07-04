69d018c99869cf4291c94b952c22e8b43a53e1a3
sibu02 <sibaprasad.tofan@gmail.com>
2024-07-04 15:48:04 +0530

    modified files

--- Array.js
+++ Array.js
@@ -1,19 +1,19 @@
 ///  Array is the collection of items.
-// //arrey is mutable.
-// let num=[50,46,87,95,64,23];
-// num[2]=44;
-// console.log(num);
-// typeof num=oject.
-// num.length=6
+//arrey is mutable.
+let num=[50,46,87,95,64,23];
+num[2]=44;
+console.log(num);
+typeof num=oject.
+num.length=6
 
 // array indices(arr[0],arr[1],arr[2]...)
 
 
 ///loop over array.
-// let arr=[40,51,87,64,98,99];
-// for(let i=0;i<arr.length;i++){
-//     console.log(arr[i]);
-// }
+let arr=[40,51,87,64,98,99];
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
 
 //for of loop.
 let states=["Ap","Mp","Delhi","Goa"];
