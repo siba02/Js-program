e79b9ba8c939731e78b4d9ff99ed37b150d08453
sibu02 <sibaprasad.tofan@gmail.com>
2024-07-20 22:04:37 +0530

    changes

--- Dom.js
+++ Dom.js
@@ -11,6 +11,30 @@ window.console.log("hello");
 // both these are same because window is a global object not javascript's object.
 
 
-// DOM(document object model).
+///DOM(document object model).
 // when a web page is loaded,the browser creates a Document Object Model of the page.
-console.dir(window.document);
+// console.dir(window.document);  // dir because it is a object.
+
+// document.body.style.background="blue"; example-to change the page dynamically. 
+
+/// DOM Manupulation.
+/// selecting with id.
+
+// document.getElementById("example");
+
+let button=document.getElementById("myid");    //id's are generally denoted by # symbols.
+console.dir(button);
+
+///selecting with class.
+
+// document.getElementsByClassName("myclass"); It return a htlm collection.
+
+let headings=document.getElementsByClassName("heading"); //class is usually denoted by . symbol.
+console.dir(headings);
+
+/// selecting with tagname.
+
+// document.getElementsByTagName("mytag"); 
+
+let paragraphs=document.getElementsByTagName("p");
+console.dir(paragraphs);
